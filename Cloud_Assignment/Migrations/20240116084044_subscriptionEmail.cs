98a897f48a20f658d8869b03d9173c19b81e5e31
eamon <csheamon1996@gmail.com>
2024-01-17 06:53:47 +0800

    eamon sns feature

---
+++ Cloud_Assignment/Migrations/20240116084044_subscriptionEmail.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cloud_Assignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class subscriptionEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NewsletterEmail",
+                columns: table => new
+                {
+                    NewsletterEmailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsletterEmail", x => x.NewsletterEmailId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsletterEmail");
+        }
+    }
+}
---
+++ Cloud_Assignment/Models/NewsletterEmail.cs
@@ -0,0 +1,15 @@
+﻿using Cloud_Assignment.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cloud_Assignment.Models
+{
+    public class NewsletterEmail
+    {
+        [Key]
+        public int NewsletterEmailId { get; set; }
+        public String? Email { get; set; }
+        [ForeignKey("User")]
+        public String? UserId { get; set; }
+    }
+}
